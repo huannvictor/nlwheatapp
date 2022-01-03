2a17f2dc6e6e9e138ba3df90bda366caaac2087a
Huann Almeida <huannvictor@gmail.com>
2022-01-02 22:19:14 -0300

    .

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { UserPhoto } from "../UserPhoto";
 
 import { styles } from "./styles";
 
@@ -8,7 +9,15 @@ import LogoSvg from "../../assets/logo.svg";
 export function Header() {
   return (
     <View style={styles.container}>
-      <Text>Sair</Text>
+      <LogoSvg />
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri="https://github.com/huannvictor.png" />
+      </View>
     </View>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 
-import { Header } from "../../components/header";
+import { Header } from "../../components/Header";
 
 import { styles } from "./styles";
 
